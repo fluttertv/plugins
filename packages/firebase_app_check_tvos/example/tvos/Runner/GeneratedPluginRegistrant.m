cb5b07121388a7d7c65c46af02e8c9ff81072980
TheNoumanDev <76642732+TheNoumanDev@users.noreply.github.com>
2026-08-14 07:06:33 +0500

    feat(firebase): add GeneratedPluginRegistrant for tvOS support

---
+++ packages/firebase_app_check_tvos/example/tvos/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,17 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+@import firebase_app_check_tvos;
+@import firebase_core_tvos;
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FirebaseAppCheckPlugin registerWithRegistrar:[registry registrarForPlugin:@"FirebaseAppCheckPlugin"]];
+  [FLTFirebaseCorePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseCorePlugin"]];
+}
+
+@end
